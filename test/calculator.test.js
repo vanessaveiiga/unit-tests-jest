c9fbebab71a4a06a1f7d77891b9dd81ae24676ba
vanessaveiiga <vanessaveiiga@gmail.com>
2022-09-24 10:32:03 -0300

    Aula 24-09

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,17 @@
-const { somar } = require("../src/calculator");
+const { somar, diminuir, multiplicar, dividir } = require("../src/calculator");
 
 test("Somar dois valores válidos", async () => {
   expect(somar(5, 10)).toStrictEqual(15);
+});
+
+test("Diminuir dois valores válidos", async () => {
+  expect(diminuir(5, 10)).toStrictEqual(-5);
+});
+
+test("Multiplicar dois valores válidos", async () => {
+  expect(multiplicar(5, 10)).toStrictEqual(50);
+});
+
+test("Dividir dois valores válidos", async () => {
+  expect(dividir(10, 5)).toStrictEqual(2);
 });
